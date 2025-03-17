60b2c2703322321cff86014263fc928fe904ce2c
Dyvergente <31512907+yulydelgado@users.noreply.github.com>
2025-03-17 15:08:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 <div>Teachable Machine Image Model</div>
-<button type="button" onclick="init()">Start</button>
+
 <div id="webcam-container"></div>
 <div id="label-container"></div>
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js"></script>
